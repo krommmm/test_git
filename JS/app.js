7412cc2e88a53b16a5b0535105e81b4ae789a99e
thibault <krom@gmx.fr>
2023-02-12 13:09:33 +0000

    base

--- JS/app.js
+++ JS/app.js
@@ -3,9 +3,5 @@ var h1 = document.querySelector("h1");
 var para = document.querySelector("p");
 
 
-<<<<<<< HEAD
-h1.style.color="red";
-=======
-h1.style.color="purple";
->>>>>>> red
 
+h1.style.color="red";
